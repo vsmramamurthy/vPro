2a07a16c2c13b9dcaa9fa478c6d9b363d65bb8e5
Administrator <Administrator@scsbwin-800150.ITTPA.EBIZ.VERIZON.COM>
2015-09-29 01:12:35 -0700

    Suresh Latest 1:45

--- WebContent/js/grouptabs.js
+++ WebContent/js/grouptabs.js
@@ -43,10 +43,12 @@ Ext.onReady(function () {
         items: [{
 					xtype: 'grouptabpanel',
 					activeGroup: 0,
+					activeItem: 2,
 					items: [{
 								mainItem: 1,
+								
 								items: [{
-											title: 'Usage',
+											title: 'MyHub Stats',
 											iconCls: 'x-icon-tickets',
 											tabTip: 'Tickets tabtip',
 											//border: false,
@@ -55,7 +57,7 @@ Ext.onReady(function () {
 											height: null
 										}, {
 											xtype: 'portalpanel',
-											title: 'Account Info',
+											title: 'Verizon Hub',
 											tabTip: 'Dashboard tabtip',
 											border: false,
 											items: [{
@@ -63,138 +65,17 @@ Ext.onReady(function () {
 												items: []
 											}]
 										}, {
-												title: 'Bills',
+												title: 'vTrack',
 												iconCls: 'x-icon-subscriptions',
 												tabTip: 'Subscriptions tabtip',
 												style: 'padding: 10px;',
+												active : true,
 												border: false,
 												layout: 'fit',
 												items: [{
-													xtype: 'tabpanel',
-													activeTab: 0,
-													items: [{
-														title: 'Nested Tabs',
-														html: Ext.example.shortBogusMarkup
-													}]
-												}]
-										}, {
-											title: 'Upgrade Eligiblity',
-											iconCls: 'x-icon-users',
-											tabTip: 'Users tabtip',
-											style: 'padding: 10px;',
-											html: Ext.example.shortBogusMarkup
-										}]
-						}, {
-							expanded: true,
-							items: [{
-								title: 'Tech Info',
-								iconCls: 'x-icon-configuration',
-								tabTip: 'Configuration tabtip',
-								style: 'padding: 10px;',
-								html:  Ext.example.shortBogusMarkup
-							}, {
-								title: 'Send Email',
-								iconCls: 'x-icon-templates',
-								tabTip: 'Templates tabtip',
-								style: 'padding: 10px;',
-								border: false,
-								items: {
-					                title: 'New Email',
-					                layout: 'fit',
-					                frame: true,
-					                border: false,
-					                height : 875,
-					                
-					                items: {
-					                    xtype: 'form',
-					                    layout:'absolute',
-					                    url:'',
-					                    padding: '5 5 0 5',
-					                    border: false,
-					                    cls: 'absolute-form-panel-body',
-					                    
-					                    defaultType: 'textfield',
-					                    items: [{
-					                        x: 0,
-					                        y: 5,
-					                        xtype: 'label',
-					                        text: 'From:'
-					                    },{
-					                        x: 55,
-					                        y: 0,
-					                        name: 'from',
-					                        hideLabel: true,
-					                        anchor:'100%'  // anchor width by %
-					                    },{
-					                        x: 0,
-					                        y: 32,
-					                        xtype: 'label',
-					                        text: 'To:'
-					                    },{
-					                        x: 55,
-					                        y: 27,
-					                        width: 69,
-					                        xtype: 'button',
-					                        text: 'Contacts'
-					                    },{
-					                        x: 127,
-					                        y: 27,
-					                        name: 'to',
-					                        hideLabel: true,
-					                        anchor: '100%'  // anchor width by %
-					                    },{
-					                        x: 0,
-					                        y: 59,
-					                        xtype: 'label',
-					                        text: 'Subject:'
-					                    },{
-					                        x: 55,
-					                        y: 54,
-					                        name: 'subject',
-					                        hideLabel: true,
-					                        anchor: '100%'  // anchor width by %
-					                    },{
-					                        x: 0,
-					                        y: 81,
-					                        hideLabel: true,
-					                        xtype: 'textarea',
-					                        name: 'msg',
-					                        anchor: '100% 100%'  // anchor width and height
-					                    }]
-					                },
-
-					                dockedItems: [
-					                    {
-					                        xtype: 'toolbar',
-					                        border: false,
-					                        cls: 'absolute-form-panel-body',
-					                        items: [
-					                            {
-					                                 text: 'Send',
-					                                 iconCls: 'icon-send'
-					                            },'-',{
-					                                 text: 'Save',
-					                                 iconCls: 'icon-save'
-					                            },{
-					                                 text: 'Check Spelling',
-					                                 iconCls: 'icon-spell'
-					                            },'-',{
-					                                 text: 'Print',
-					                                 iconCls: 'icon-print'
-					                            },'->',{
-					                                 text: 'Attach a File',
-					                                 iconCls: 'icon-attach'
-					                            }
-					                        ]
-					                    }
-					                ]
-					            }
-							} ]
-						}, {
-							expanded: false,
-							items: {
-								title: 'Track',
+								title: 'VerizonHub Dashboard',
 								bodyPadding: 10,
+								
 								items : {
 					                layout: 'fit',
 					                frame: true,
@@ -443,7 +324,14 @@ Ext.onReady(function () {
 										]
 										}
 								}
-							}
+							}]
+										}, {
+											title: 'Upgrade Eligiblity',
+											iconCls: 'x-icon-users',
+											tabTip: 'Users tabtip',
+											style: 'padding: 10px;',
+											items : []
+										}]
 						}]
 					}]
     });
